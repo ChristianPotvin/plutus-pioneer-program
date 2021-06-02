b9d9a0a409685b3c0688da5c9a02be3af1088a3a
Christian Potvin <4276662+ChristianPotvin@users.noreply.github.com>
2021-06-01 23:39:19 -0400

    Week 5 Homework

--- code/week05/src/Week05/Homework2.hs
+++ code/week05/src/Week05/Homework2.hs
@@ -20,6 +20,7 @@ import           Plutus.Contract        as Contract hiding (when)
 import           Plutus.Trace.Emulator  as Emulator
 import qualified PlutusTx
 import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
+import           Plutus.V1.Ledger.Ada   (adaToken)
 import           Ledger                 hiding (singleton)
 import           Ledger.Constraints     as Constraints
 import qualified Ledger.Typed.Scripts   as Scripts
@@ -35,20 +36,46 @@ import           Wallet.Emulator.Wallet
 -- Minting policy for an NFT, where the minting transaction must consume the given UTxO as input
 -- and where the TokenName will be the empty ByteString.
 mkPolicy :: TxOutRef -> ScriptContext -> Bool
-mkPolicy oref ctx = True -- FIX ME!
+mkPolicy oref ctx = traceIfFalse "UTxO not consumed"   hasUTxO           &&
+                    traceIfFalse "wrong amount minted" checkMintedAmount
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo ctx
+
+    hasUTxO :: Bool
+    hasUTxO = any (\i -> txInInfoOutRef i == oref) $ txInfoInputs info
+
+    checkMintedAmount :: Bool
+    checkMintedAmount = case flattenValue (txInfoForge info) of
+        [(cs, _, amt)] -> cs  == ownCurrencySymbol ctx && amt == 1
+        _              -> False
 
 policy :: TxOutRef -> Scripts.MonetaryPolicy
-policy oref = undefined -- IMPLEMENT ME!
+policy oref = mkMonetaryPolicyScript $
+    $$(PlutusTx.compile [|| Scripts.wrapMonetaryPolicy . mkPolicy ||])
+    `PlutusTx.applyCode`
+    PlutusTx.liftCode oref
 
 curSymbol :: TxOutRef -> CurrencySymbol
-curSymbol = undefined -- IMPLEMENT ME!
+curSymbol oref = scriptCurrencySymbol $ policy oref
 
 type NFTSchema =
     BlockchainActions
         .\/ Endpoint "mint" ()
 
 mint :: Contract w NFTSchema Text ()
-mint = undefined -- IMPLEMENT ME!
+mint = do
+    pk    <- Contract.ownPubKey
+    utxos <- utxoAt (pubKeyAddress pk)
+    case Map.keys utxos of
+        []       -> Contract.logError @String "no utxo found"
+        oref : _ -> do
+            let val     = Value.singleton (curSymbol oref) adaToken 1
+                lookups = Constraints.monetaryPolicy (policy oref) <> Constraints.unspentOutputs utxos
+                tx      = Constraints.mustForgeValue val <> Constraints.mustSpendPubKeyOutput oref
+            ledgerTx <- submitTxConstraintsWith @Void lookups tx
+            void $ awaitTxConfirmed $ txId ledgerTx
+            Contract.logInfo @String $ printf "forged %s" (show val)
 
 endpoints :: Contract () NFTSchema Text ()
 endpoints = mint' >> endpoints
